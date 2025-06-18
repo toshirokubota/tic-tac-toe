dad11e73c72f71347556e96d2d8621f25fe95181
Toshiro Kubota <toshiro732@hotmail.com>
2025-06-18 10:03:50 -0400

    minor update for accessibility

--- src/components/InitialSetup.tsx
+++ src/components/InitialSetup.tsx
@@ -39,15 +39,17 @@ export default function InitialSetup({setPlayers, start}:
                     <div className="radio-group my-4">
                         <label htmlFor="player-mark-x">
                             <input type='radio' id='player-mark-x' name='player-mark' value='x'
-                                onChange={handleChange}/>
-                            <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+                                onChange={handleChange}
+                                aria-label="choose X as player 1's mark"/>
+                            <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg" role='img' aria-label='X icon'>
                                 <path d="M15.002 1.147 32 18.145 48.998 1.147a3 3 0 0 1 4.243 0l9.612 9.612a3 3 0 0 1 0 4.243L45.855 32l16.998 16.998a3 3 0 0 1 0 4.243l-9.612 9.612a3 3 0 0 1-4.243 0L32 45.855 15.002 62.853a3 3 0 0 1-4.243 0L1.147 53.24a3 3 0 0 1 0-4.243L18.145 32 1.147 15.002a3 3 0 0 1 0-4.243l9.612-9.612a3 3 0 0 1 4.243 0Z"/>
                             </svg>
                         </label>
                         <label htmlFor="player-mark-o">
                             <input type='radio' id='player-mark-o' name='player-mark' value='o'
-                                onChange={handleChange}/>
-                            <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg">
+                                onChange={handleChange}
+                                aria-label="choose O as player 1's mark"/>
+                            <svg width="64" height="64" xmlns="http://www.w3.org/2000/svg" role='img' aria-label='O icon'>
                             <path d="M32 0c17.673 0 32 14.327 32 32 0 17.673-14.327 32-32 32C14.327 64 0 49.673 0 32 0 14.327 14.327 0 32 0Zm0 18.963c-7.2 0-13.037 5.837-13.037 13.037 0 7.2 5.837 13.037 13.037 13.037 7.2 0 13.037-5.837 13.037-13.037 0-7.2-5.837-13.037-13.037-13.037Z"/>
                             </svg>
                         </label>                        
